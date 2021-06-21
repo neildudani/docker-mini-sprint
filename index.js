0a71f6d494e2db8fb82348405725f83f5d3ec978
Neil Dudani <neil.dudani@gmail.com>
2021-06-21 09:36:15 -0400

    Ran and pushed docker image to docker hub

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const express = require('express');
+const express = require('express')
 const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-});
+  res.send('Visit Canada!')
+})
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
-});
+})
